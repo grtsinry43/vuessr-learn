c59c92081ae348a308daa50db78a1fd02ac1aa68
grtsinry43 <grtsinry43@outlook.com>
2024-08-05 00:33:29 +0800

    fix: 代码混淆与样式测试

--- src/app.vue
+++ src/app.vue
@@ -2,15 +2,37 @@
 import {ref} from "vue";
 
 const count = ref(0)
+const msg = ref('Hello Vue SSR')
 </script>
 
 <template>
   <div class="app-container">
-    <h1>Counter: {{ count }}</h1>
-    <button @click="count++">Increment</button>
+    <h1 class="title">Counter: {{ count }}</h1>
+    <button class="increase" @click="count++">Increment</button>
+    <input class="input" v-model="msg"/>
+    <h2 class="msg">{{ msg }}</h2>
   </div>
 </template>
 
 <style scoped>
+.app-container {
+  text-align: center;
+}
 
+.app-container .title {
+  color: red;
+}
+
+.increase {
+  margin-top: 20px;
+}
+
+.input {
+  outline: 2px #f40;
+}
+
+.msg {
+  color: #ccc;
+  font-weight: bold;
+}
 </style>
